1fd5a6f2151ddbb093b3d852d9a022ad8ae7abd5
Natasha Murashev <nmurashev@gmail.com>
2013-05-28 15:22:18 -0500

    added pin to current location

--- FlickSquare/ViewController.m
+++ FlickSquare/ViewController.m
@@ -14,6 +14,9 @@
     CLLocationManager *locationMananger;
 }
 
+-(void)showCurrentLocation:(CLLocation *)location;
+- (void)addPinToLocation:(CLLocationCoordinate2D)location;
+
 @end
 
 @implementation ViewController
@@ -37,10 +40,37 @@
 
 #pragma mark - Location manager
 
+-(void)showCurrentLocation:(CLLocation *)location
+{
+    MKCoordinateSpan span;
+    span.latitudeDelta = 0.015;
+    span.longitudeDelta = 0.015;
+    
+    CLLocationCoordinate2D center = CLLocationCoordinate2DMake(location.coordinate.latitude, location.coordinate.longitude);
+    
+    MKCoordinateRegion region;
+    region.span = span;
+    region.center=center;
+    
+    [mapView setRegion:region animated:TRUE];
+    [mapView regionThatFits:region];
+    
+    [self addPinToLocation:center];
+}
+
+- (void)addPinToLocation:(CLLocationCoordinate2D)location
+{
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    [annotation setCoordinate:location];
+    [mapView addAnnotation:annotation];
+}
+
 - (void)locationManager:(CLLocationManager *)manager didUpdateLocations:(NSArray *)locations
 {
     CLLocation *location = locations[0];
 
+    [self showCurrentLocation:location];
+    
     [self getFoursquareVenuesWithLatitude:(CGFloat)location.coordinate.latitude
                              andLongitude:(CGFloat)location.coordinate.longitude];
     
