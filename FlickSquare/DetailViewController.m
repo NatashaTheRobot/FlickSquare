6ec3ad5c753a4d89616ac83632e0ac62c97e6792
Jesse Tello <jesse.tello@gmail.com>
2013-05-29 15:46:52 -0500

    added images to image array

--- FlickSquare/DetailViewController.m
+++ FlickSquare/DetailViewController.m
@@ -9,6 +9,10 @@
 #import "DetailViewController.h"
 
 @interface DetailViewController ()
+{
+    NSMutableArray* imageArray;
+}
+
 
 @end
 
@@ -28,6 +32,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     self.navigationItem.title = self.venue.name;
+    
+    imageArray = [NSMutableArray array];
 }
 
 
@@ -41,7 +47,7 @@
 
 - (void)getFlickrPhotos
 {
-    NSString* flickrURLString = [NSString stringWithFormat:@"%@&method=flickr.photos.search&api_key=%@&bbox=%f,%f,%f,%f&format=json&nojsoncallback=1",FLICKR_BASE_URL, FLICKR_API_KEY, self.venue.longitude - .05, self.venue.latitude - .05,self.venue.longitude + .05,self.venue.latitude + .05];
+    NSString* flickrURLString = [NSString stringWithFormat:@"%@&method=flickr.photos.search&api_key=%@&bbox=%f,%f,%f,%f&format=json&nojsoncallback=1",FLICKR_BASE_URL, FLICKR_API_KEY, self.venue.longitude - .01, self.venue.latitude - .01,self.venue.longitude + .01,self.venue.latitude + .01];
     
     NSURL* url = [NSURL URLWithString:flickrURLString];
     
@@ -68,6 +74,11 @@
                                                          [photoDictionary objectForKey:@"secret"]];
                                        
                                        NSLog(@"%@",photoURLString);
+                                       
+                                       UIImage* image = [[UIImage alloc] initWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:photoURLString]]];
+                                       
+                                       [imageArray addObject:image];
+                                       
                                        
                                    }
                                    
