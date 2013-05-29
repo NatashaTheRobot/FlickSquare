3abdcc36e7aea7803b1773e20a5520b894b5ef45
Jesse Tello <jesse.tello@gmail.com>
2013-05-29 11:56:18 -0500

    added venue annotations

---
+++ FlickSquare/VenueAnnotation.h
@@ -0,0 +1,21 @@
+//
+//  VenueAnnotation.h
+//  FlickSquare
+//
+//  Created by Jesse Tello on 5/29/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MapKit/MapKit.h>
+
+@interface VenueAnnotation : NSObject <MKAnnotation>
+
+@property (nonatomic, assign) CLLocationCoordinate2D coordinate;
+
+@property (nonatomic, strong) NSString* title;
+
+@property (nonatomic, strong) NSString* subtitle;
+
+
+@end
